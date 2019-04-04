32d86621bda2bf04ca3d0116dfadcdf688ab25b3
thibault genaitay <tgenaitay@gmail.com>
2019-04-04 18:20:56 +0800

    fixing link

--- config.rb
+++ config.rb
@@ -82,7 +82,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
 
   # Optional Settings
-  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  deploy.remote = 'https://github.com/tgenaitay/workshop-ikea-mp-2h.git' # remote name or git url, default: origin
   # deploy.branch = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
